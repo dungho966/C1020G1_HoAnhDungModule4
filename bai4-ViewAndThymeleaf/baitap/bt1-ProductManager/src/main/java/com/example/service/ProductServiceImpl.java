981df5c26d54b2ca077c313bf2e1f59b9686af1b
unknown <dunghoanh1996@gmail.com>
2021-02-25 20:53:55 +0700

    update

---
+++ bai4-ViewAndThymeleaf/baitap/bt1-ProductManager/src/main/java/com/example/service/ProductServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.service;
+
+import com.example.model.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductServiceImpl implements ProductService {
+
+    private static Map<Integer , Product> productList ;
+
+    static {
+        productList = new HashMap<>();
+        productList.put(1 , new Product(1 , "ip6" , 100 , "apple"));
+        productList.put(2 , new Product(2 , "ip7" , 200 , "apple"));
+        productList.put(3 , new Product(3 , "ip8" , 300 , "apple"));
+        productList.put(4 , new Product(4 , "ip9" , 400 , "apple"));
+        productList.put(5 , new Product(5 , "ip10" , 500 , "apple"));
+        productList.put(6 , new Product(6 , "ip11" , 600 , "apple"));
+    }
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<Product>(productList.values());
+    }
+
+    @Override
+    public void save(Product product) {
+        productList.put(product.getId() , product) ;
+    }
+
+    @Override
+    public Product findById(int id) {
+        return productList.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        productList.put(id , product) ;
+    }
+
+    @Override
+    public void remove(int id) {
+        productList.remove(id) ;
+    }
+}
